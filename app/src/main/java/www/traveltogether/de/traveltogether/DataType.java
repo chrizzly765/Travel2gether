b2444c777ecf7391768bc0e38f74ca79d56ac2d0
Anna-Lena Müller <Anna-Lena Mller>
2016-05-27 13:59:07 +0200

    added stuff

--- app/src/main/java/www/traveltogether/de/traveltogether/DataType.java
+++ app/src/main/java/www/traveltogether/de/traveltogether/DataType.java
@@ -4,5 +4,5 @@ package www.traveltogether.de.traveltogether;
  * Created by Anna-Lena on 16.05.2016.
  */
 public enum DataType {
-    TRIP, TASK, EXPENSE, PACKING, ACTIVITY, PARTICIPANT
+    TRIP, TASK, EXPENSE, PACKING, ACTIVITY, PARTICIPANT, REGISTER, LOGIN
 }
--- app/src/main/java/www/traveltogether/de/traveltogether/model/Statistics.java
+++ app/src/main/java/www/traveltogether/de/traveltogether/model/Statistics.java
@@ -1,7 +1,12 @@
 package www.traveltogether.de.traveltogether.model;
 
+import java.text.SimpleDateFormat;
+
 /**
  * Created by Anna-Lena on 12.05.2016.
  */
 public class Statistics {
+    private int commonPercentage = 0;
+    private int personalPercentage = 0;
+    private int daysLeft = 0;
 }
